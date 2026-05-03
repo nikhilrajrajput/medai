426679ed68bff39cdbf242e51d03ceb0aea90b3c
nikhilrajrajput <21mci1126@cuchd.in>
2026-05-04 04:46:04 +0530

    updated gmail connection

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const nodemailer = require('nodemailer');
 const dotenv= require('dotenv')
 
@@ -7,7 +9,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   port: process.env.EMAIL_PORT,
-  secure: true, // Use true for port 465, false for 587
+  secure: false, // Use true for port 465, false for 587
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
