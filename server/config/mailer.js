72baafd88b1ca25f4061e193237deb80b03a5eba
nikhilrajrajput <21mci1126@cuchd.in>
2026-05-04 04:14:12 +0530

    updated gmail connection

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   port: process.env.EMAIL_PORT,
   secure: true, // Use true for port 465, false for 587
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // Your 16-character App Password
