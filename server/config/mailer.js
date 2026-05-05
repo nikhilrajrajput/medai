54ec49ef195a08f35636eb6c1cff7c17d423f88f
nikhilrajrajput <21mci1126@cuchd.in>
2026-05-05 22:29:05 +0530

    updated nodemailer

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -21,12 +21,8 @@ const transporter = nodemailer.createTransport({
   pool: true,
   maxConnections: 3,
   maxMessages: 50,
-  connectionTimeout: 15000,
-  greetingTimeout: 15000,
-  socketTimeout: 20000,
   tls: {
     rejectUnauthorized: false, // Needed on some cloud platforms with strict TLS
-    minVersion: 'TLSv1.2',
   },
 });
 
